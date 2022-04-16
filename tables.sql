dea476799173be4e04b31f5b6806fa41189647be
Zirnael <mr.tostek@gmail.com>
2022-04-16 12:08:31 +0200

    Tables
    Added constraints

--- tables.sql
+++ tables.sql
@@ -8,8 +8,8 @@ CREATE TABLE IF NOT EXISTS public."Students"
     "StudentID" text NOT NULL,
     "FirstName" text NOT NULL,
     "LastName" text NOT NULL,
-    "Pesel" text NOT NULL,
-    "Semester" smallint NOT NULL,
+    "Pesel" text NOT NULL CONSTRAINT peselLength check ( length("Pesel") = 11 ),
+    "Semester" smallint NOT NULL CONSTRAINT correctSemester check ( "Semester" > 0 AND "Semester" <= 10 ),
     PRIMARY KEY ("StudentID")
 );
 
@@ -18,14 +18,14 @@ CREATE TABLE IF NOT EXISTS public."Lecturers"
     "LecturerID" smallserial NOT NULL,
     "FirstName" text NOT NULL,
     "LastName" text NOT NULL,
-    "Degree" text NOT NULL,
+    "Degree" text NOT NULL CONSTRAINT correctDegree check ( "Degree" = ANY(ARRAY ['BA','MA','PhD','DCL','Mgr','Dr','Inz'])),
     PRIMARY KEY ("LecturerID")
 );
 
 CREATE TABLE IF NOT EXISTS public."Faculties"
 (
     "FacultyID" smallserial NOT NULL,
-    "Name" text NOT NULL,
+    "Name" text NOT NULL UNIQUE,
     PRIMARY KEY ("FacultyID")
 );
 
@@ -35,21 +35,21 @@ CREATE TABLE IF NOT EXISTS public."Courses"
     "Name" text NOT NULL,
     "FacultyID" smallserial NOT NULL,
     "LecturerID" smallserial NOT NULL,
-    "NumberOfPlaces" smallint NOT NULL,
-    "ETCS" smallint NOT NULL,
+    "NumberOfPlaces" smallint NOT NULL CONSTRAINT positiveNOPlaces check ( "NumberOfPlaces" > 0 ),
+    "ETCS" smallint NOT NULL CONSTRAINT correctECTS check ( "ETCS" > 0 AND "ETCS" < 10),
     "Description" text,
-    "Semester" smallint NOT NULL,
+    "Semester" smallint NOT NULL CONSTRAINT correctSemester check ( "Semester" > 0 and "Semester" <= 10 ),
     PRIMARY KEY ("CourseID")
 );
 
-CREATE TABLE IF NOT EXISTS public."Students_Faculties"
+CREATE TABLE IF NOT EXISTS public."StudentsFaculties"
 (
     "StudentID" text NOT NULL,
     "FacultyID" smallserial NOT NULL,
     PRIMARY KEY ("StudentID", "FacultyID")
 );
 
-CREATE TABLE IF NOT EXISTS public."Students_Courses"
+CREATE TABLE IF NOT EXISTS public."StudentsCourses"
 (
     "StudentID" text NOT NULL,
     "CourseID" smallserial NOT NULL,
@@ -59,7 +59,7 @@ CREATE TABLE IF NOT EXISTS public."Students_Courses"
 CREATE TABLE IF NOT EXISTS public."CoursesHours"
 (
     "CourseID" smallserial NOT NULL,
-    "WeekDay" smallint NOT NULL,
+    "WeekDay" smallint NOT NULL check ( "WeekDay" < 5 ),
     "StartTime" time without time zone NOT NULL,
     "EndTime" time without time zone NOT NULL,
     PRIMARY KEY ("CourseID", "WeekDay", "StartTime")
@@ -81,7 +81,7 @@ ALTER TABLE IF EXISTS public."Courses"
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."Students_Faculties"
+ALTER TABLE IF EXISTS public."StudentsFaculties"
     ADD FOREIGN KEY ("StudentID")
     REFERENCES public."Students" ("StudentID") MATCH SIMPLE
     ON UPDATE NO ACTION
@@ -89,7 +89,7 @@ ALTER TABLE IF EXISTS public."Students_Faculties"
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."Students_Faculties"
+ALTER TABLE IF EXISTS public."StudentsFaculties"
     ADD FOREIGN KEY ("FacultyID")
     REFERENCES public."Faculties" ("FacultyID") MATCH SIMPLE
     ON UPDATE NO ACTION
@@ -97,7 +97,7 @@ ALTER TABLE IF EXISTS public."Students_Faculties"
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."Students_Courses"
+ALTER TABLE IF EXISTS public."StudentsCourses"
     ADD FOREIGN KEY ("StudentID")
     REFERENCES public."Students" ("StudentID") MATCH SIMPLE
     ON UPDATE NO ACTION
@@ -105,7 +105,7 @@ ALTER TABLE IF EXISTS public."Students_Courses"
     NOT VALID;
 
 
-ALTER TABLE IF EXISTS public."Students_Courses"
+ALTER TABLE IF EXISTS public."StudentsCourses"
     ADD FOREIGN KEY ("CourseID")
     REFERENCES public."Courses" ("CourseID") MATCH SIMPLE
     ON UPDATE NO ACTION
@@ -121,3 +121,4 @@ ALTER TABLE IF EXISTS public."CoursesHours"
     NOT VALID;
 
 END;
+
---
+++ values.sql
@@ -0,0 +1,6 @@
+begin;
+insert into "Lecturers" ("FirstName", "LastName", "Degree") VALUES ('Maciej','Kowalski','Mgr');
+insert into "Lecturers" ("FirstName", "LastName", "Degree") VALUES ('Jan','Nowak','Dr');
+insert into "Lecturers" ("FirstName", "LastName", "Degree") VALUES ('Andrzej','Kowalski','Inz');
+
+end;
